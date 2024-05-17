a6fe0500317b38a949f7e0a6b3ac1334f579a130
CBMHessels <kees@loanscanada.ca>
2024-05-16 20:31:46 -0600

    change theme

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::view('404', '404');
+Route::view('/', 'index');
+Route::view('/index', 'index');
+Route::view('/about_us', 'about_us');
+Route::view('/index_2', 'index_2');
+Route::view('/index_3', 'index_3');
+Route::view('/account', 'account');
+Route::view('/blog', 'blog');
+Route::view('/blog/details', 'blog_details');
+Route::view('/cart/checkout', 'cart_checkout');
+Route::view('/contact', 'contact');
+Route::view('/element/accordions', 'elements-accordions');
+Route::view('/element/alerts', 'elements-alerts');
+Route::view('/element/counters', 'elements-counters');
+Route::view('/element/form', 'elements-form');
+Route::view('/element/icons', 'elements-icons');
+Route::view('/element/tables/info', 'elements-tables_info');
+Route::view('/element/tables/pricing', 'elements-tables_pricing');
+Route::view('/element/embeds/media', 'elements-media_embeds');
+
+Route::view('/element/tabs', 'elements-tabs');
+Route::view('/faqs', 'faq');
+Route::view('/gallery/classic', 'gallery_classic');
+Route::view('/gallery/masonry', 'gallery_masonry');
+
+Route::view('/products', 'products');
+Route::view('/product/details', 'product_details');
+Route::view('/product/single', 'product_single');
+
+Route::view('/services', 'services');
+Route::view('/catalog/shop', 'catalog_shop');
+Route::view('/typography', 'typography');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
