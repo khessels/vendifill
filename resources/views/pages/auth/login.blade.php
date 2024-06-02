08f401ea87d0347a21ea88c51762ce5c4d0a2c82
CBMHessels <kees@loanscanada.ca>
2024-06-02 16:08:24 -0600

    integrate strapi

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -26,11 +26,67 @@
 @endsection
 
 @section('start-screen')
-    {!! $content[$page]['start_screen'] ?? '' !!}
+{{--    {!! $content[$page]['start_screen'] ?? '' !!}--}}
 @endsection
 
 @section('content')
-    {!! $content[$page]['main'] ?? '' !!}
+{{--    {!! $content[$page]['main'] ?? '' !!}--}}
+    <main role="main">
+        <!-- start section -->
+        <section class="section section--gray-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <div style="width:50%">
+                            <!-- start auth form -->
+                            <form class="auth-form" action="/login" method="POST">
+                                @csrf
+                                <h4>{{$content['login']['sign_in'] ?? 'sign_in'}}</h4>
+
+                                <div class="input-wrp">
+                                    <input name='email' class="textfield"
+                                           placeholder="{{$content['login']['email'] ?? 'email'}}" type="text"/>
+                                </div>
+
+                                <div class="input-wrp">
+                                    <i class="textfield-ico fontello-eye"></i>
+                                    <input name="password" class="textfield"
+                                           placeholder="{{$content['login']['password'] ?? 'password'}}"
+                                           type="password"/>
+                                </div>
+
+                                <div class="row justify-content-between no-gutters pt-1">
+                                    <div class="col-auto">
+                                        <label class="checkfield align-bottom">
+                                            <input type="checkbox" checked="">
+                                            <i></i>
+                                            {{$content['login']['remember_me'] ?? 'remember_me'}}
+                                        </label>
+                                    </div>
+
+                                    <div class="col-auto">
+                                        <a class="__link"
+                                           href="/recovery">{{$content['login']['forgot_password'] ?? 'forgot_password'}}</a>
+                                    </div>
+                                </div>
+
+                                <button class="custom-btn custom-btn--big custom-btn--s3" type="submit"
+                                        role="button">{{$content['login']['btn_sign_in'] ?? 'btn_sign_in'}}</button>
+
+                                <div class="text-center"><a
+                                        href="/signup">{{$content['login']['link_sign_up'] ?? 'link_sign_up'}}</a> {{$content['login']['sign_up_postfix'] ?? 'sign_up_postfix'}}
+                                </div>
+                            </form>
+
+                        </div>
+                        <!-- end auth form -->
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!-- end section -->
+    </main>
 @stop
 
 @section('footer')
